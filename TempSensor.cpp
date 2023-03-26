d3c6b75bcd7bea8d448e3adf120469becf6713f0
Tedd OKANO <okano.akifumi@gmail.com>
2023-03-27 06:10:21 +0900

    new base class: LM75B

--- TempSensor.cpp
+++ TempSensor.cpp
@@ -10,9 +10,43 @@ TempSensor::~TempSensor()
 {
 }
 
+LM75B::LM75B( I2C &i2c_, char i2c_address ) : TempSensor( i2c_, i2c_address )
+{
+	//  do nothing.
+	//  leave it in default state.
+}
+
+LM75B::~LM75B()
+{
+}
+
+float LM75B::temp()
+{
+	return read_r16( Temp ) / 256.0;
+}
+
+void LM75B::thresholds( float v0, float v1 )
+{
+	float higher	= (v0 < v1) ? v1 : v0;
+	float lower		= (v0 < v1) ? v0 : v1;
+	
+	write_r16( Tos,   ((uint16_t)(higher * 256.0)) & 0xFF80 );
+	write_r16( Thyst, ((uint16_t)(lower  * 256.0)) & 0xFF80 );
+}
+
+void LM75B::os_mode( mode flag )
+{
+	uint16_t	v;
+	
+	v	 = read_r8( Conf );
+	v	&= ~0x02;
+	v	|= flag << 1;
+	
+	write_r8( Conf, v );
+}
 
 
-PCT2075::PCT2075( I2C &i2c_, char i2c_address ) : TempSensor( i2c_, i2c_address )
+PCT2075::PCT2075( I2C &i2c_, char i2c_address ) : LM75B( i2c_, i2c_address )
 {
 	//  do nothing.
 	//  leave it in default state.
--- TempSensor.h
+++ TempSensor.h
@@ -31,7 +31,64 @@ public:
 	virtual float temp( void );
 };
 
-/** TempSensor class
+
+/** LM75B class
+ *	
+ *  @class LM75B
+
+ *  About LM75B:
+ *    https://www.nxp.jp/products/sensors/ic-digital-temperature-sensors/digital-temperature-sensor-and-thermal-watchdog:LM75B
+ */
+
+class LM75B : public TempSensor
+{
+public:
+	/** Name of the PCT2075 registers */
+	enum reg_num {
+		Temp,	/**< Temp register	*/
+		Conf,	/**< Conf register	*/
+		Thyst,	/**< Thyst register	*/
+		Tos,	/**< Tos registe	*/
+	};
+	enum mode {
+		COMPARATOR,	/**< Comparator mode	*/
+		INTERRUPT,	/**< Interrupt mode	*/
+	};
+
+	/** Create a PCT2075 instance connected to specified I2C pins with specified address
+	 *
+	 * @param i2c_address I2C-bus address (default: (0x90>>1))
+	 */
+	LM75B( I2C &i2c_, char i2c_address = (0x90 >> 1) );
+
+	/** Destructor of PCT2075
+	 */
+	~LM75B();
+	
+	/** Get temperature value in degree Celsius [°C] 
+	 *
+	 * @return temperature value in degree Celsius [°C] 
+	 */
+	float temp( void );
+	
+	/** Set Overtemperature shutdown threshold (Tos) and hysteresis (Thyst) in degree Celsius [°C] 
+	 *
+	 *	This method takes 2 values and higher value will set as the threshold (Tos) and 
+	 *. another will be the hysteresis (Thyst)
+	 *
+	 * @param v0 a value in degree Celsius
+	 * @param v1 a value in degree Celsius
+	 */	
+	void thresholds( float v0, float v1 );
+
+	/** Set OS operation mode 
+	 *
+	 * @param flag use PCT2075::COMPARATOR or PCT2075::INTERRUPT values
+	 */	
+	void os_mode( mode flag );
+};
+
+/** PCT2075 class
  *	
  *  @class PCT2075
 
@@ -39,7 +96,7 @@ public:
  *    https://www.nxp.jp/products/sensors/ic-digital-temperature-sensors/ic-bus-fm-plus-1-degree-c-accuracy-digital-temperature-sensor-and-thermal-watchdog:PCT2075
  */
 
-class PCT2075 : public TempSensor
+class PCT2075 : public LM75B
 {
 public:
 	/** Name of the PCT2075 registers */
